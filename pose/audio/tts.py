45c33f17069a26af3b8832c0dde24862272894df
Gunchul Song <gunchul77@gmail.com>
2022-01-16 12:27:06 +1100

    audio 100

--- pose/audio/tts.py
+++ pose/audio/tts.py
@@ -1,28 +1,34 @@
 import requests
 import urllib
+import os
 
-def gen(text, file):
+def str_get(num):
+    result = None
+    str_1 = {'0':'', '1':'하나', '2':'둘', '3':'셋', '4':'넷', '5':'다섯', '6':'여섯', '7':'일곱', '8':'여덟', '9':'아홉' }
+    str_2 = {'0':'', '1':'열', '2':'스물', '3':'서른', '4':'마흔', '5':'쉰', '6':'여슨', '7':'일흔', '8':'여순', '9':'하흔' }
+
+    if (num == 100):
+        return "백"
+
+    str_num = str(num)
+
+    if len(str_num) == 1:
+        result = str_1[str_num[-1]]
+    elif len(str_num) == 2:
+        result = str_2[str_num[-2]] + str_1[str_num[-1]]
+    return result
+
+def file_gen(num):
+    file_name = f'{num}.mp3'
+    if os.path.exists(file_name):
+        return
+    text = str_get(num)
     q = {'q':text}
     str = urllib.parse.urlencode(q)
     url = f'http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&{str}&tl=ko'
     r = requests.get(url, allow_redirects=True)
-    with open(f'{file}.mp3', 'wb') as f:
+    with open(f'{file_name}', 'wb') as f:
         f.write(r.content)
 
-texts = ["하나", "둘", "셋", "넷", "다섯", "여섯", "일곱", "여덟", "아홉", "열",
-         "열하나", "열둘", "열셋", "열넷", "열다섯", "열여섯", "열일곱", "열여덟", "열아홉", "스물",
-         "스물하나", "스물둘", "스물셋", "스물넷", "스물다섯", "스물여섯", "스물일곱", "스물여덟", "스물아홉", "서른"]
-
-# for i, text in enumerate(texts):
-#     gen(text, str(i+1))
-
-# -----------------------------------------
-
-def gen_html():
-    for i in range(1,31):
-        print(f'<audio id="audio_{i}">')
-        print(f'<source src="sound/{i}.mp3" type="audio/mp3">')
-        print(f'</audio>')
-
-gen_html()
-
+for num in range(1,101):
+    file_gen(num)
